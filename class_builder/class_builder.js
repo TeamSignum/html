3be393b9e3303c32814b99ed8ef7f34f1de0a572
Daniel Cushing <cushing.daniel.james@gmail.com>
2015-11-22 13:56:24 -0700

    Improved Class Builder
    Class Builder - Added toolbar
    Class Builder - Added copy from toolbar
    Class Builder - Improved event handling
    Map Manager - Improved functionality. Added methods.
    Moved mongo_test.php into examples folder

--- class_builder/class_builder.js
+++ class_builder/class_builder.js
@@ -4,42 +4,121 @@
  * Date: 11/21/15 
  */ 
 
+ function copyNode(canvas, node){
+	 var radius = node.radius; 
+ 	 var fill = node.fill; 
+ 	 var top = node.top;
+ 	 var left = node.left; 
+ 	 var id = node.id; 
 
-// Class Builder Global Variable 
-var CB = {}; 
+ 	 var n = new fabric.Circle({
+ 	 	radius: radius, 
+ 	 	fill : fill, 
+ 	 	top : top, 
+ 	 	left : left, 
+ 	 	id: id 
+ 	 }); 
 
-/*
- * CB Manager Constructor  
- */ 
- CB.Manager = function(){
+     n.hasControls = false; 
+     n.hasBorders  = false; 
 
- 	// Init Manger
- 	this.nodes = []; 
- 	this.edges = []; 
- 	this.tools = []; 
-
+     canvas.add(n);  
  }
 
-/* 
- * CB Node constructor. 
- * @param: node - KineticJS circle. 
- */ 
-CB.Node = function(node){
-	this.x           = node.x;  
-	this.y           = node.y; 
-	this.radius      = node.radius; 
-	this.fill        = node.fill;  
-	this.stroke      = node.stroke; 
-	this.strokeWidth = node.strokeWidth; 
-	this.draggable   = node.draggable; 
-}
+ function copyEdge(canvas, edge){
+ 	var x1 = edge.x1; 
+ 	var x2 = edge.x2; 
+ 	var y1 = edge.y1; 
+ 	var y2 = edge.y2; 
+ 	var fill = edge.fill; 
+ 	var stroke = edge.stroke; 
+ 	var strokeWidth = edge.strokeWidth; 
+ 	var id = edge.id; 
+ 	var dotted; 
+ 	var e; 
 
-/* 
- * Add input node to this class builder manager.  
- * @param: _node - KineticJS circle. 
- */ 
- CB.Manager.addNode = function(_node) {
- 	node = new CB.Node(_node); 
- 	this.nodes.push(node);
- 	return node; 
+ 	if (id === "tb_lineDotted"){
+ 		dotted = edge.strokeDashArray; 
+ 	}
+
+ 	e = new fabric.Line([x1, y1, x2, y2], {
+		fill: fill, 
+		stroke: stroke, 
+		strokeWidth: strokeWidth, 
+		strokeDashArray: dotted,
+		id: id
+ 	}); 
+
+ 	e.hasControls = true; 
+ 	e.hasBorders  = true; 
+
+ 	canvas.add(e); 
  }
+
+$( document ).ready(function() {
+
+	var mngr = new MM.Manager(); 
+
+	var canvas = new fabric.Canvas('map');
+	canvas.add(new fabric.Circle({ radius: 50, fill: '#fff', top: 50,  left: 50, id: 'tb_largeCircle' }));
+	canvas.add(new fabric.Circle({ radius: 40, fill: '#fff', top: 200, left: 55, id: 'tb_mediumCircle' }));
+	canvas.add(new fabric.Circle({ radius: 30, fill: '#fff', top: 320, left: 60, id: 'tb_smallCircle' }));
+
+	canvas.add(new fabric.Line([60, 400, 120, 500], { fill: 'red', stroke: 'red', strokeWidth: 3, id: 'tb_lineSolid' }));
+	canvas.add(new fabric.Line([60, 525, 120, 625], { fill: 'red', stroke: 'red', strokeWidth: 3, strokeDashArray: [5, 5], id: 'tb_lineDotted' }));
+
+	canvas.item(0).hasControls = canvas.item(0).hasBorders = false;
+	canvas.item(1).hasControls = canvas.item(0).hasBorders = false;
+	canvas.item(2).hasControls = canvas.item(0).hasBorders = false;
+	canvas.item(3).hasControls = canvas.item(0).hasBorders = false;
+	canvas.item(4).hasControls = canvas.item(0).hasBorders = false;
+
+	canvas.add(new fabric.Line([180, 0, 180, 1000], { fill: 'black', stroke: 'black', strokeWidth: 4 }));
+	canvas.item(5).lockMovementX = canvas.item(5).lockMovementY = true; 
+	canvas.item(5).selectable = canvas.item(5).hasControls = canvas.item(5).hasBorders = false; 
+
+	canvas.on({
+
+	  'mouse:down': function(e) {
+	    if (e.target) {
+	    	e.target.opacity = 0.5;
+
+		    if(e.target.id === "tb_largeCircle" || e.target.id === "tb_mediumCircle" || e.target.id === "tb_smallCircle"){
+		      copyNode(canvas, e.target);  
+		      e.target.id = "mapNode";  
+		      mngr.addNode(e.target); 
+		    }
+
+		    else if(e.target.id === "tb_lineSolid" || e.target.id === "tb_lineDotted"){
+		    	copyEdge(canvas, e.target); 
+		    	e.target.id = "mapEdge"; 
+		      	mngr.addEdge(e.target); 
+		    }
+
+	      	canvas.renderAll();
+	    }
+	  },
+
+	  'mouse:up': function(e) {
+	    if (e.target) {
+
+	      if(e.target.left < 180){
+	      	canvas.remove(e.target); 
+	      }
+	      
+	      e.target.opacity = 1;
+	      canvas.renderAll();
+	    }
+	  },
+
+	  'object:moved': function(e) {
+	    e.target.opacity = 0.5;
+	  },
+
+	  'object:modified': function(e) {
+	    e.target.opacity = 1;
+	  }
+
+	});
+
+});
--- class_builder/class_builder.php
+++ class_builder/class_builder.php
@@ -3,54 +3,18 @@
 <head> 
   <link rel="import" href="../imports/import.html">
   <link rel="stylesheet" type="text/css" href="class_builder.css"> 
+  <script src="../map_manager/map_manager.js" type="text/javascript"></script>
   <script src="class_builder.js" type="text/javascript"></script>
   <title> Class Builder </title>  
 </head> 
 <body>
 
 <h1>Class Builder<h1>
+<h2> Learning Map </h2>
 
 <div id="container"> 
-	<h2> Learning Map </h2>
-  <canvas id="map" width="500" height="500"></canvas>
+  <canvas id="map" width="1900" height="1000"></canvas>
 </div>
-
-<script type="text/javascript"> 
-
-	$( document ).ready(function() {
-
-  var canvas = new fabric.Canvas('map');
-  canvas.add(new fabric.Circle({ radius: 30, fill: '#f55', top: 100, left: 100 }));
-  canvas.item(0).hasControls = canvas.item(0).hasBorders = false;
-
-  canvas.on({
-    'mouse:down': function(e) {
-      if (e.target) {
-        e.target.opacity = 0.5;
-        canvas.renderAll();
-        alert("Event!"); 
-      }
-    },
-    'mouse:up': function(e) {
-      if (e.target) {
-        e.target.opacity = 1;
-        canvas.renderAll();
-        alert("Event!"); 
-      }
-    },
-    'object:moved': function(e) {
-      e.target.opacity = 0.5;
-    },
-    'object:modified': function(e) {
-      e.target.opacity = 1;
-    }
-
-  });
-
-  this.__canvases.push(canvas);
-  });
-
-</script> 
 
 </body>
 </html>
