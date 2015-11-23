7a128cf31b4ede6ee6b4d9621230cff03bf7d399
probablyjoe <probablyjoe@gmail.com>
2015-11-22 22:35:59 -0700

    Update profile.js

--- profile/profile.js
+++ profile/profile.js
@@ -1,1 +1,5 @@
-place holder
+angular.module('getUserProfile', []).controller('getUserProfileCtrl', function($scope) {
+    $scope.firstName= "John";
+    $scope.lastName= "Doe";
+	$scope.email= "u000000@utah.edu";
+});
