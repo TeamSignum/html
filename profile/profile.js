94096836357aef272bf0f45ecd947db00c2502dd
MedianJoe <probablyjoe@gmail.com>
2015-11-24 13:07:47 -0700

    Revert "Skeleton for page, no functionality"
    This reverts commit 7a2e93573ccdbecbaee297a9c5e404eada1dd571.

---
+++ profile/profile.js
@@ -0,0 +1,5 @@
+angular.module('getUserProfile', []).controller('getUserProfileCtrl', function($scope) {
+    $scope.firstName= "John";
+    $scope.lastName= "Doe";
+	$scope.email= "u000000@utah.edu";
+});
