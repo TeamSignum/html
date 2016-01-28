26489552307e0c07bc954149f246d15819b33df3
SsiBalTang <ckzks2013@gmail.com>
2016-01-27 20:34:37 -0700

    Registration & Test upload
    need to do registration and test upload

--- registration/registration.js
+++ registration/registration.js
@@ -76,3 +76,19 @@ $(".previous").click(function(){
 $(".submit").click(function(){
   return false;
 })
+
+// here is for Upload the pic
+function readURL(input){
+  if(input.files && input.files[0]){
+    var reader = new FileReader();
+
+    reader.onload = function(e){
+      $('#blah')
+        .attr('src', e.target.result)
+        .width(150)
+        .height(200);
+    };
+
+    reader.readAsDataURL(input.files[0]);
+  }
+}
