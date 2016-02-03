12336074ed39f2a45e871944d6c3509a312a693f
MedianJoe <probablyjoe@gmail.com>
2016-02-02 21:37:18 -0700

    Grades page front and back end complete

--- grades/grades.js
+++ grades/grades.js
@@ -1,43 +1,91 @@
 /*           
  * Author: Joseph Cottongim                         
- * Date: February 1, 2016                                                        
+ * Created: February 1, 2016  
  *            
  * Javascript file for handling the interaction between                                                        
- * grades.html, grades.php, and the database for the 
- * Learning Universe website.
+ * grades.html, grades.php to build the grade view.
  *          
  */  
  
 $( document ).ready(function() 
 {
-	// Function for toggling the grade displays (NEEDS TO BE IN DOCUMENT.READY OR IT WON'T BIND)
-	$(".trigger").click(function(){
-    $(this).next(".toggle").slideToggle("slow");
-  });
-	
-	//getGrades();
-	//insertGrades();
+	getGrades();
 });
 
 function getGrades(){
-	"use strict";
 	$.ajax({
 		async: true,
 		type: 'POST',
 		url: "grades.php",
+		data: {'gradeQuery': '3'}, // Query number for obtaining all grades
 		datatpye: "json",
 		success: function(result){
-			//var parsedResult = JSON.parse(result);
 			//alert(result);
+			var parsedResult = JSON.parse(result);
+
+			buildDisplayPage(parsedResult);
 		}
 	});
 }
 
+function buildDisplayPage(parsedResult){
+	var currentClass = '';
+	var html = '';
+	var gradeTotal = 0;
+	var gradeCount = 0;
+	var grade = 0;
+	
+	for(var i = 0; i < parsedResult.length; i++){
+		if(currentClass !== parsedResult[i].classnumber){
+			currentClass = parsedResult[i].classnumber;
+			// Build the trigger header for the class
+			html +='<div class = "trigger"><span class="textPadding">+ ' + currentClass + ' - PLACEHOLDER%</span></div><div class = "toggle">';
+			// Insert the grade into the toggle panel
+			html += '<div class = "assignment"><span class="textPadding">' + parsedResult[i].title + ' - ' + parsedResult[i].score + '%</span></div>';
+			gradeTotal += parseInt(parsedResult[i].score);
+			gradeCount++;
+		}
+		else
+		{
+			// Insert the remaining grades into the toggle panel
+			html += '<div class = "assignment"><span class="textPadding">' + parsedResult[i].title + ' - ' + parsedResult[i].score + '%</span></div>';
+			gradeTotal += parseInt(parsedResult[i].score);
+			gradeCount++;
+		}
+		
+		if(i < parsedResult.length-1){
+			if(currentClass !== parsedResult[i+1].classnumber){
+				html +='</div>';
+				// Calculate the overall class grade
+				grade = (gradeTotal/gradeCount).toFixed(2);
+				// Insert the grade into the html string
+				html = html.replace("PLACEHOLDER", grade);
+				// Reset the grade variables for the next class
+				gradeTotal = 0;
+				gradeCount = 0;
+			}
+		}
+		else{
+			html +='</div>';
+			// Calculate the overall class grade
+			grade = (gradeTotal/gradeCount).toFixed(2);
+			// Insert the grade into the html string
+			html = html.replace("PLACEHOLDER", grade);
+		}
+	}
+	
+	insertGrades(html);
+}	
 
+function insertGrades(html){
+		$('#grades').html(html);
+		bindHtmlElements();
+}
 
-function insertGrades(){
-		//$('#gradeContainer').html('');
+function bindHtmlElements(){
+	// Function for toggling the grade displays
+	$(".trigger").click(function(){
+    $(this).next(".toggle").slideToggle("medium");
+  });
 }
 	
-function buildDisplayPage(){
-}	
--- grades/grades.php
+++ grades/grades.php
@@ -6,9 +6,27 @@
 	 *
 	 * PHP file for queries relating to grades on the Learning Universe website
 	 *
+	 * 
 	 */
 
 	session_start();
+	
+	// Check SESSION variables
+	if(!isset($_SESSION['uid']) || !isset($_SESSION['role'])){
+		; // TODO: redirect to the login page
+	}
+	else{
+		$uid = $_SESSION['uid'];
+		$role = $_SESSION['role'];
+	}
+	
+	// Check POST variables
+	if(isset($_POST['classid'])){
+		$classid = $_POST['classid'];
+	}
+	if(isset($_POST['nid'])){
+		$nid = $_POST['nid'];
+	}
 	
 	if(isset($_POST['gradeQuery']))
 	{
@@ -17,54 +35,139 @@
 		
 		// Set include path and require appropriate files
 		set_include_path( "../");
-		require_once (db.php);
+		require_once ('db.php');
 		
-		// Login to the database
-		openDB();
+		// Login to the database	
+		$DB=openDB();
 		
 		// Queries are numbered 1-7
 		switch($gradeQuery) 
 		{
 			// Student requests
 			case 1: // Query a grade for a single assignment
-				$query = "SELECT score FROM grades WHERE uid='$uid' AND nid='$nid' AND classid='$classid'";
+				$query = "SELECT c.classnumber, n.title, g.score
+									FROM grades g
+									INNER JOIN nodes n
+										ON g.nid=n.nid
+									INNER JOIN classes c
+										ON g.classid=c.id
+									WHERE g.uid=? and g.classid=? and n.nid=?;";
+									
+				$statement = $DB->prepare($query);
+				$statement->bindParam(1, $uid);					
+				$statement->bindValue (2, $classid);
+				$statement->bindValue (3, $nid);
+				break;
 			case 2: // Query all grades for a class
-				$query = "SELECT score FROM grades WHERE uid='$uid' AND classid='$classid'";
+				$query = "SELECT c.classnumber, n.title, g.score
+									FROM grades g
+									INNER JOIN nodes n
+										ON g.nid=n.nid
+									INNER JOIN classes c
+										ON g.classid=c.id
+									WHERE g.uid=? and g.classid=?
+									ORDER BY n.nid ASC;";
+				
+				$statement = $DB->prepare($query);
+				$statement->bindValue (1, $uid);
+				$statement->bindValue (2, $classid);
+				break;
 			case 3: // Query all grades for all classes
-				$query = "SELECT score FROM grades WHERE uid='$uid' ORDER BY classid";
+				$query =	"SELECT c.classnumber, n.title, g.score
+									FROM grades g
+									INNER JOIN nodes n
+										ON g.nid=n.nid
+									INNER JOIN classes c
+										ON g.classid=c.id
+									WHERE g.uid=?
+									ORDER BY c.classnumber ASC;";
+									
+				$statement = $DB->prepare($query);
+				$statement->bindParam(1, $uid);		
+				break;
 			
 			// Professor requests
 			case 4: // Query a single student grade for a single assignment
-				$query = "SELECT score FROM grades WHERE uid='$uid' AND nid='$nid' AND classid='$classid'";
+				$query =	"SELECT c.classnumber, n.title, g.score
+									FROM grades g
+									INNER JOIN nodes n
+										ON g.nid=n.nid
+									INNER JOIN classes c
+										ON g.classid=c.id
+									WHERE g.uid=? and n.nid=?;";
+				
+				$statement = $DB->prepare($query);
+				$statement->bindParam(1, $uid);					
+				$statement->bindValue (2, $nid);
+				break;
 			case 5: // Query all grades for a student for a specific class
-				$query = "SELECT score FROM grades WHERE uid='$uid' AND classid='$classid'";
+				$query =	"SELECT c.classnumber, n.title, g.score
+									FROM grades g
+									INNER JOIN nodes n
+										ON g.nid=n.nid
+									INNER JOIN classes c
+										ON g.classid=c.id
+									WHERE g.uid=? and g.classid=?
+									ORDER BY n.nid ASC;";
+									
+				$statement = $DB->prepare($query);
+				$statement->bindParam(1, $uid);					
+				$statement->bindValue (2, $classid);
+				break;
 			case 6: // Query all grades for an assignment
-				$query = "SELECT score FROM grades WHERE nid='$nid' AND classid='$classid'";
+				$query =	"SELECT c.classnumber, n.title, g.score
+									FROM grades g
+									INNER JOIN nodes n
+										ON g.nid=n.nid
+									INNER JOIN classes c
+										ON g.classid=c.id
+									WHERE n.nid=?;";
+									
+				$statement = $DB->prepare($query);
+				$statement->bindParam(1, $nid);					
+				break;
 			case 7: // Query all grades for a class
-				$query = "SELECT score FROM grades WHERE classid='$classid' ORDER BY uid ASC, nid ASC";
+				$query = "SELECT c.classnumber, n.title, g.score
+									FROM grades g
+									INNER JOIN nodes n
+										ON g.nid=n.nid
+									INNER JOIN classes c
+										ON g.classid=c.id
+									WHERE g.uid=? and g.classid=?
+									ORDER BY n.nid ASC;";
+									
+				$statement = $DB->prepare($query);
+				$statement->bindParam(1, $uid);					
+				$statement->bindValue (2, $classid);
+				break;
 		}
 		
 		if($query!=null)
 		{
-			$statement = $DB->prepare($query);
-			
 			// Execute the database query
 			$statement->execute();
 			$result = $statement->fetchAll(PDO::FETCH_ASSOC);
-				
-			// Need handling for failed queries
+			
+			// Check for a failed query
+			if(mysql_errno()){
+    		echo "MySQL error ".mysql_errno().": "
+         .mysql_error()."\n<br>When executing <br>\n$query\n<br>";
+			}
+			else {
+				echo(json_encode($result));
+			}
 			
 			// Close the PDO DB Connection (not necessary, but good practice...so I've read)
 			$DB = null;
 		}
 		else
 		{
-			$result="query error";
+			echo "ERROR: Invalid query type.";
 		}
 	}
 	else
 	{
-		$result="query error";
+		echo "ERROR: No query type provided.";
 	}
 
 ?>
