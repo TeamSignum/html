256b8e1a9d1e5fd47965773844cae4b4dea6910d
SsiBalTang <ckzks2013@gmail.com>
2016-02-03 20:02:18 -0700

    Resigtration page

--- registration/registration.js
+++ registration/registration.js
@@ -3,45 +3,100 @@ var current_fs, next_fs, previous_fs; //fieldsets
 var left, opacity, scale; //fieldset properties which we will animate
 var animating; //flag to prevent quick multi-click glitches
 
+var fieldsetn = 1;
+
 $(".next").click(function(){
-  if(animating) return false;
-  animating = true;
+  var error = 0;
+
+  if(fieldsetn == 1){
+    var emailVal = $('#email').val();
+    var emailReg = /^([\w-\.]+@utah.edu)?$/;
+
+     // Email check
+     if(emailVal == ""){
+        $('#email').addClass('warning');
+        $('#email').attr("placeholder", "Empty, type your Umail address");
+        error = 1;
+     }else if(!emailReg.test(emailVal)){
+        $('#email').addClass('warning');
+        $('#email').attr("placeholder", "Type your correct @utah.edu");
+        error = 1;
+     }else{
+      $('#email').removeClass('warning');
+     }
+
+    // password check
+    var passVal1 = $('#pass').val();
+    var passVal2 = $('#pass-check').val();
+
+    if(passVal1 == "" && passVal2 == ""){
+      $('#pass').addClass('warning');
+      $('#pass-check').addClass('warning');
+      $('#pass').attr("placeholder", "Empty, type your password");
+      $('#pass-check').attr("placeholder", "Empty, type your password");
+      error = 1;
+    }else if(passVal1 != passVal2){
+      $('#pass').addClass('warning');
+      $('#pass-check').addClass('warning');
+      $('#pass').attr("placeholder", "Password does not match");
+      $('#pass-check').attr("placeholder", "Password does not match");
+      error = 1;
+    }else if(passVal1.length < 5){
+      $('#pass').addClass('warning');
+      $('#pass-check').addClass('warning');
+      $('#pass').attr("placeholder", "Password has to be at least 6 character");
+      $('#pass-check').attr("placeholder", "Password has to be at least 6 character");
+      error = 1;
+    }else{
+      $('#pass').removeClass('warning');
+      $('#pass-check').removeClass('warning');
+    }
+
+  }else if(fieldsetn == 2){
+    
+  }
   
-  current_fs = $(this).parent();
-  next_fs = $(this).parent().next();
-  
-  //activate next step on progressbar using the index of next_fs
-  $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-  
-  //show the next fieldset
-  next_fs.show(); 
-  //hide the current fieldset with style
-  current_fs.animate({opacity: 0}, {
-    step: function(now, mx) {
-      //as the opacity of current_fs reduces to 0 - stored in "now"
-      //1. scale current_fs down to 80%
-      scale = 1 - (1 - now) * 0.2;
-      //2. bring next_fs from the right(50%)
-      left = (now * 50)+"%";
-      //3. increase opacity of next_fs to 1 as it moves in
-      opacity = 1 - now;
-      current_fs.css({'transform': 'scale('+scale+')'});
-      next_fs.css({'left': left, 'opacity': opacity});
-    }, 
-    duration: 800, 
-    complete: function(){
-      current_fs.hide();
-      animating = false;
-    }, 
-    //this comes from the custom easing plugin
-    easing: 'easeInOutBack'
-  });
+
+  if(error == 0){
+    if(animating) return false;
+    animating = true;
+    fieldsetn = fieldsetn +1;
+    current_fs = $(this).parent();
+    next_fs = $(this).parent().next();
+    
+    //activate next step on progressbar using the index of next_fs
+    $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+    
+    //show the next fieldset
+    next_fs.show(); 
+    //hide the current fieldset with style
+    current_fs.animate({opacity: 0}, {
+      step: function(now, mx) {
+        //as the opacity of current_fs reduces to 0 - stored in "now"
+        //1. scale current_fs down to 80%
+        scale = 1 - (1 - now) * 0.2;
+        //2. bring next_fs from the right(50%)
+        left = (now * 50)+"%";
+        //3. increase opacity of next_fs to 1 as it moves in
+        opacity = 1 - now;
+        current_fs.css({'transform': 'scale('+scale+')'});
+        next_fs.css({'left': left, 'opacity': opacity});
+      }, 
+      duration: 800, 
+      complete: function(){
+        current_fs.hide();
+        animating = false;
+      }, 
+      //this comes from the custom easing plugin
+      easing: 'easeInOutBack'
+    });
+  }
 });
 
 $(".previous").click(function(){
   if(animating) return false;
   animating = true;
-  
+  fieldsetn = fieldsetn - 1;
   current_fs = $(this).parent();
   previous_fs = $(this).parent().prev();
   
@@ -76,19 +131,3 @@ $(".previous").click(function(){
 $(".submit").click(function(){
   return false;
 })
-
-// here is for Upload the pic
-function readURL(input){
-  if(input.files && input.files[0]){
-    var reader = new FileReader();
-
-    reader.onload = function(e){
-      $('#blah')
-        .attr('src', e.target.result)
-        .width(150)
-        .height(200);
-    };
-
-    reader.readAsDataURL(input.files[0]);
-  }
-}
