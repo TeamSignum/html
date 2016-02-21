8755184799ae5c240ce2707bc07285e5da3b76f6
MedianJoe <probablyjoe@gmail.com>
2016-02-21 00:39:05 -0700

    Updated profile layout and fixed image upload

---
+++ profile/editProfile.php
@@ -0,0 +1,229 @@
+<?php
+
+/* 
+ * Author: Joseph Cottongim
+ * Date: February 3, 2016
+ * 
+ * Description: php file to save user profile changes on the Learning
+ * Universe website.
+ *
+ */
+
+session_start();
+
+// Check that uid is set in the Session
+if(isset($_SESSION['uid'])){	
+	$uid = $_SESSION['uid'];
+}
+else{
+	die();
+}
+
+if(isset($_POST['queryType'])){
+	$queryType=$_POST['queryType'];
+}
+else{
+	die('Query type required to update profile was not found.');
+}
+
+// Set include path and require appropriate files
+set_include_path( "../");
+require_once ('db.php');
+
+switch ($queryType) {
+	case 'updateEmail':
+		if(isset($_POST['email'])){
+			$email=$_POST['email'];
+		}
+		else{
+			die("New email address was not found.");
+		}
+
+		try{
+			// Login to the database	
+			$DB=openDB();
+
+			$query = "UPDATE users
+					  SET email=?
+					  WHERE uid=?";
+							
+			$statement = $DB->prepare($query);
+			$statement->bindValue(1, $email);					
+			$statement->bindValue(2, $uid);
+
+			$statement->execute();
+
+			// Update the email session variable
+			$_SESSION['email']=$email;
+		}
+		catch(PDOException $ex){
+			if($ex->getCode()=="23000"){
+				echo("Email address already taken.");
+			}
+			else{
+			  	echo
+			     "<p>oops</p>
+			      <p> Code: {$ex->getCode()} </p>
+			      <pre>$ex</pre>";
+			}
+			die();
+		}
+		
+		break;
+
+	case 'updateUserId':
+		if(isset($_POST['userid'])){
+			$userid=$_POST['userid'];
+		}
+		else{
+			die("New User ID was not found.");
+		}
+
+		try{
+			// Login to the database	
+			$DB=openDB();
+
+			$query = "UPDATE users
+					  SET uid=?
+					  WHERE uid=?";
+							
+			$statement = $DB->prepare($query);
+			$statement->bindValue(1, $userid);					
+			$statement->bindValue(2, $uid);
+
+			$statement->execute();
+
+			// Update the uid session variable
+			$_SESSION['uid']=$userid;
+		}
+		catch(PDOException $ex){
+			if($ex->getCode()=="23000"){
+				echo("User ID already taken.");
+			}
+			else{
+			  	echo
+			     "<p>oops</p>
+			      <p> Code: {$ex->getCode()} </p>
+			      <pre>$ex</pre>";
+			}
+			die();
+		}
+		
+		break;
+
+	case 'updateName':
+		if(isset($_POST['firstname']) && isset($_POST['lastname'])){
+			$firstname=$_POST['firstname'];
+			$lastname=$_POST['lastname'];
+		}
+		else{
+			die("New name information was not found.");
+		}
+
+		try{
+			// Login to the database	
+			$DB=openDB();
+
+			$query = "UPDATE users
+					  SET firstname=?, lastname=?
+					  WHERE uid=?";
+							
+			$statement = $DB->prepare($query);
+			$statement->bindValue(1, $firstname);					
+			$statement->bindValue(2, $lastname);
+			$statement->bindValue(3, $uid);
+
+			$statement->execute();
+
+			// Update the uid session variable
+			$_SESSION['firstname']=$firstname;
+			$_SESSION['lastname']=$lastname;
+		}
+		catch(PDOException $ex){
+			if($ex->getCode()=="23000"){
+				echo("User ID already taken.");
+			}
+			else{
+			  	echo
+			     "<p>oops</p>
+			      <p> Code: {$ex->getCode()} </p>
+			      <pre>$ex</pre>";
+			}
+			die();
+		}
+		break;
+
+	case 'updatePicture':
+		if(!isset($_FILES['imageToUpload'])){
+			die("No picture found.");
+		}
+
+		// $check if the file is an image.
+		$check = getimagesize($_FILES["imageToUpload"]["tmp_name"]);
+		if($check === false) {
+		      die("Uploaded file is not an image.");
+		}
+
+		// Get image file prefix
+		$prefix=($_FILES['imageToUpload']['name']);
+		// Set target directory
+		$targetDirectory="../profile_images/";
+		// Create a unique file name with tempnam function
+		$destinationFileName = tempnam($targetDirectory, $prefix);
+		
+		// Assuming $_FILES['file']['error'] == 0 (no errors)
+		if (move_uploaded_file($_FILES['imageToUpload']['tmp_name'], $destinationFileName)) {
+		    // Get the extension from uploaded file
+		    $fileExtension = '.' . pathinfo($_FILES['imageToUpload']['name'], PATHINFO_EXTENSION);
+		    // Replace tmp with actual file extension
+		    $newFileName=str_replace('.tmp', $fileExtension, $destinationFileName);
+		    // update the file name
+		    rename($destinationFileName, $newFileName);
+		    // Strip the file path to store the image name in the database
+		    $databaseFileName=basename($newFileName);
+
+		    // File uploaded successfully, add reference to the database
+		    try{
+			// Login to the database	
+			$DB=openDB();
+
+			$query = "UPDATE users
+					  SET profilepic=?
+					  WHERE uid=?";
+							
+			$statement = $DB->prepare($query);
+			$statement->bindValue(1, $databaseFileName);	
+			$statement->bindValue(2, $uid);
+			
+			$statement->execute();
+
+			echo("Success: " . $databaseFileName);
+
+			// TODO:  Need to delete the old file...
+			}
+			catch(PDOException $ex){
+				if($ex->getCode()=="23000"){
+					echo("User ID already taken.");
+				}
+				else{
+				  	echo
+				     "<p>oops</p>
+				      <p> Code: {$ex->getCode()} </p>
+				      <pre>$ex</pre>";
+				}
+			die();
+		}
+
+		} else {
+		    // tempnam() created a new file, but moving the uploaded file failed
+		    unlink($destinationFileName); // remove temporary file
+		    die('File could not be uploaded.');
+		}
+		break;
+	
+	default:
+		die("Invalid query type provided.");
+		break;
+}	
+	
+?>
--- profile/profile.js
+++ profile/profile.js
@@ -11,10 +11,15 @@ $( document ).ready(function()
 {
 	getProfileData();
 	
-	// This prevents the profileform from submitting and refreshing the page.
-	$("#profileForm").submit(function(e){
-    e.preventDefault();
-  });
+	// Prevent default action of forms submitting
+	$('form').submit(function(e){
+    	e.preventDefault();
+    });
+
+	emailDefaultView();
+	userIdDefaultView();
+	nameDefaultView();
+	pictureDefaultView();
 });
 
 // Queries the database and stores profile information in javascript variables
@@ -26,11 +31,13 @@ function getProfileData(){
 		datatpye: "json",
 		success: function(result){
 			//alert(result);
+			// Parse query results into variables
 			var parsedResult = JSON.parse(result);
 			_userid = parsedResult[0].uid;
 			_email = parsedResult[0].email;
 			_firstName = parsedResult[0].firstname;
 			_lastName = parsedResult[0].lastname;
+
 			if(parsedResult[0].profilepic === null){
 				_profileimage = "../profile_images/default.jpg";
 			}
@@ -38,78 +45,322 @@ function getProfileData(){
 				_profileimage="../profile_images/" + parsedResult[0].profilepic;
 				//alert(_profileimage);
 			}
-			profileView();
-			bindHtmlElements();
+
+			// Place actual variables into the html fields
+			$('#email').val(_email);
+			$('#userid').val(_userid);
+			$('#firstname').val(_firstName);
+			$('#lastname').val(_lastName);
+			$('#profileimage').attr('src', _profileimage);
+			
+			// Code to setup a listener on the image to allow previews
+			$('#imageToUpload').change(function(event){
+				var tmppath = URL.createObjectURL(event.target.files[0]);
+				$('#profileimage').attr('src', tmppath);
+			});
 		}
 	});
 }
 
-// Submits profile information to the database
-function submitProfileChanges(){
-	if(validateFormData)
+// Logic for individual profile forms
+function emailDefaultView(){
+	// Set up 'CHANGE' button
+	$('#edit-email-button').off().on('click', emailEditView);
+	$('#edit-email-button').attr('class', 'btn btn-primary btn-sm');
+	$('#edit-email-button').text('CHANGE');
+	// Set up 'CANCEL' button
+	$('#cancel-edit-email-button').hide();
+	$('#cancel-edit-email-button').off().on('click', emailDefaultView);
+	$('#email').val(_email);
+	// Disable email input field
+	$('#email').prop('disabled',true);
+}
+
+function emailEditView(){
+	// Update "CHANGE' button
+	$('#edit-email-button').off().on('click', submitEmailChange);
+	$('#edit-email-button').attr('class', 'btn btn-success btn-sm');
+	$('#edit-email-button').text('SUBMIT');
+	// Display 'CANCEL' button
+	$('#cancel-edit-email-button').show();
+	// Enable email input field
+	$('#email').prop('disabled',false);
+	
+	turnOffOtherEditFields($('#edit-email-button'));
+}
+
+function submitEmailChange(){
+	// Use html5 validation
+	if($('#email')[0].checkValidity())
 	{
 		// Get the form information and create a FormData object
-		var form = document.getElementById("profileForm");
+		var form = $('#email-form')[0];
 		var formData = new FormData(form);
 		
+		// Disable buttons while the update is taking place
+		$('#edit-email-button').prop('disabled',true);
+		$('#cancel-edit-email-button').prop('disabled',true);
+
+		// Append information to tell php what function to run
+		formData.append("queryType", 'updateEmail');
+					
 		//insert into database, handle any submission errors.
 		$.ajax({
 			async: true,
 			processData: false, // This must be false with FormData object
-      		contentType: false, // This must be false with FormData object
+	    	contentType: false, // This must be false with FormData object
 			type: 'POST',
-			url: "saveProfile.php",
+			url: "editProfile.php",
 			data: formData,
 			success: function(result){
 				//alert(result);				
-				// Reload the profileForm to get the new data
-				getProfileData();
+				if(result==''){
+					// successful update, activate fields
+					_email=$('#email').val();
+					$('#edit-email-button').prop('disabled',false);
+					$('#cancel-edit-email-button').prop('disabled',false);
+					emailDefaultView();
+
+				}
+				else if(result=='Email address already taken.'){
+					alert("Email already in use.");
+					$('#edit-email-button').prop('disabled',false);
+					$('#cancel-edit-email-button').prop('disabled',false);
+					emailEditView();
+				}
+				else{
+					alert(result);
+				}
 			}
 		});
 	}
-	else
+}
+
+
+function userIdDefaultView(){
+	// Set up 'CHANGE' button
+	$('#edit-userid-button').off().on('click', userIdEditView);
+	$('#edit-userid-button').attr('class', 'btn btn-primary btn-sm');
+	$('#edit-userid-button').text('CHANGE');
+	// Set up 'CANCEL' button
+	$('#cancel-edit-userid-button').hide();
+	$('#cancel-edit-userid-button').off().on('click', userIdDefaultView);
+	$('#userid').val(_userid);
+	// Disable userid input field
+	$('#userid').prop('disabled',true);
+}
+
+function userIdEditView(){
+	// Update "CHANGE' button
+	$('#edit-userid-button').off().on('click', submitUserIdChange);
+	$('#edit-userid-button').attr('class', 'btn btn-success btn-sm');
+	$('#edit-userid-button').text('SUBMIT');
+	// Display 'CANCEL' button
+	$('#cancel-edit-userid-button').show();
+	// Enable userid input field
+	$('#userid').prop('disabled',false);
+	
+	turnOffOtherEditFields($('#edit-userid-button'));
+}
+
+function submitUserIdChange(){
+	// Use html5 validation
+	if($('#userid')[0].checkValidity())
 	{
-		alert("invalid data");
+		// Get the form information and create a FormData object
+		var form = $('#userid-form')[0];
+		var formData = new FormData(form);
+		
+		// Disable buttons while the update is taking place
+		$('#edit-userid-button').prop('disabled',true);
+		$('#cancel-edit-userid-button').prop('disabled',true);
+
+		// Get the form information and create a FormData object
+		var form = document.getElementById("userid-form");
+		var formData = new FormData(form);
+		// Append information to tell php what function to run
+		formData.append("queryType", 'updateUserId');
+			
+		//insert into database, handle any submission errors.
+		$.ajax({
+			async: true,
+			processData: false, // This must be false with FormData object
+	    	contentType: false, // This must be false with FormData object
+			type: 'POST',
+			url: "editProfile.php",
+			data: formData,
+			success: function(result){
+				//alert(result);				
+				if(result==''){
+					// successful update, activate fields
+					_userid=$('#userid').val();
+					$('#edit-userid-button').prop('disabled',false);
+					$('#cancel-edit-userid-button').prop('disabled',false);
+					userIdDefaultView();
+
+				}
+				else if(result=='User ID already taken.'){
+					alert("User ID already in use.");
+					$('#edit-userid-button').prop('disabled',false);
+					$('#cancel-edit-userid-button').prop('disabled',false);
+					userIdEditView();
+				}
+				else{
+					alert(result);
+				}
+			}
+		});
 	}
 }
 
-// Check if form information is in the appropriate format
-function validateFormData(){
-	return true;
-}
-
-// Displays the profile information in a non-editable format
-function profileView(){
-	// Make all fields disabled
-	$('#email').prop('disabled', true);
-	$('#userid').prop('disabled', true);
-	$('#firstname').prop('disabled', true);
-	$('#lastname').prop('disabled', true);
-	// Hide picture file input
-	$('#browsebutton').hide();
-	// Place actual values in the fields
-	$('#email').val(_email);
-	$('#userid').val(_userid);
+function nameDefaultView(){
+	// Set up 'CHANGE' button
+	$('#edit-name-button').off().on('click', nameEditView);
+	$('#edit-name-button').attr('class', 'btn btn-primary btn-sm');
+	$('#edit-name-button').text('CHANGE');
+	// Set up 'CANCEL' button
+	$('#cancel-edit-name-button').hide();
+	$('#cancel-edit-name-button').off().on('click', nameDefaultView);
 	$('#firstname').val(_firstName);
 	$('#lastname').val(_lastName);
+	// Disable userid input field
+	$('#firstname').prop('disabled',true);
+	$('#lastname').prop('disabled',true);
+}
+
+function nameEditView(){
+	// Update "CHANGE' button
+	$('#edit-name-button').off().on('click', submitNameChange);
+	$('#edit-name-button').attr('class', 'btn btn-success btn-sm');
+	$('#edit-name-button').text('SUBMIT');
+	// Display 'CANCEL' button
+	$('#cancel-edit-name-button').show();
+	// Enable userid input field
+	$('#firstname').prop('disabled',false);
+	$('#lastname').prop('disabled',false);
+	
+	turnOffOtherEditFields($('#edit-name-button'));
+}
+
+function submitNameChange(){
+	// Use html5 validation
+	if($('#firstname')[0].checkValidity() && $('#lastname')[0].checkValidity())
+	{
+		// Get the form information and create a FormData object
+		var form = document.getElementById("name-form");
+		var formData = new FormData(form);
+		// Append information to tell php what function to run
+		formData.append("queryType", 'updateName');
+
+		// Disable buttons while the update is taking place
+		$('#edit-name-button').prop('disabled',true);
+		$('#cancel-edit-name-button').prop('disabled',true);
+
+		//insert into database, handle any submission errors.
+		$.ajax({
+			async: true,
+			processData: false, // This must be false with FormData object
+	    	contentType: false, // This must be false with FormData object
+			type: 'POST',
+			url: "editProfile.php",
+			data: formData,
+			success: function(result){
+				//alert(result);
+				if(result==''){
+					// successful update, activate fields
+					_firstName=$('#firstname').val();
+					_lastName=$('#lastname').val();
+					$('#edit-name-button').prop('disabled',false);
+					$('#cancel-edit-name-button').prop('disabled',false);
+					nameDefaultView();
+
+				}
+				else{
+					alert(result);
+					$('#edit-name-button').prop('disabled',false);
+					$('#cancel-edit-name-button').prop('disabled',false);
+					nameEditView();
+				}
+			}
+		});
+	}
+}
+
+function pictureDefaultView(){
+	// Set up 'CHANGE' button
+	$('#edit-picture-button').off().on('click', pictureEditView);
+	$('#edit-picture-button').attr('class', 'btn btn-primary btn-sm');
+	$('#edit-picture-button').text('CHANGE');
+	// Set up 'CANCEL' button
+	$('#cancel-edit-picture-button').hide();
+	$('#cancel-edit-picture-button').off().on('click', pictureDefaultView);
 	$('#profileimage').attr('src', _profileimage);
-	$('#buttonregion').html('<button onclick="profileEdit()" class="btn btn-primary btn-lg btn-block">Edit Profile</button>');
+	// Hide browse button
+	$('#browse-picture').hide();
 }
 
-// Displays the profile information in and editable format
-function profileEdit(){
-	$('#email').prop('disabled', false);
-	$('#userid').prop('disabled', false);
-	$('#firstname').prop('disabled', false);
-	$('#lastname').prop('disabled', false);
-  $('#browsebutton').show();
-	$('#buttonregion').html('<button onclick="submitProfileChanges()" class="btn btn-primary btn-lg btn-block">Submit Changes</button>');	
+function pictureEditView(){
+	// Update "CHANGE' button
+	$('#edit-picture-button').off().on('click', submitPictureChange);
+	$('#edit-picture-button').attr('class', 'btn btn-success btn-sm');
+	$('#edit-picture-button').text('SUBMIT');
+	// Display 'CANCEL' button
+	$('#cancel-edit-picture-button').show();
+	// Display browse button
+	$('#browse-picture').show();
+	
+	turnOffOtherEditFields($('#edit-picture-button'));
 }
 
-function bindHtmlElements(){
-	$('#imageToUpload').change(function(event){
-		var tmppath = URL.createObjectURL(event.target.files[0]);
-		$('#profileimage').attr('src', tmppath);
-	});
+function submitPictureChange(){
+	// Check if a file was chosen
+	if($('#imageToUpload').val()==''){
+		pictureDefaultView();
+	}
+	else{
+		// Get the form information and create a FormData object
+		var form = $('#picture-form')[0];
+		var formData = new FormData(form);
+		// Append information to tell php what function to run
+		formData.append("queryType", 'updatePicture');
+
+		// Disable buttons while the update is taking place
+		$('#edit-picture-button').prop('disabled',true);
+		$('#cancel-edit-picture-button').prop('disabled',true);
+
+		//insert into database, handle any submission errors.
+		$.ajax({
+			async: true,
+			processData: false, // This must be false with FormData object
+	    	contentType: false, // This must be false with FormData object
+			type: 'POST',
+			url: "editProfile.php",
+			data: formData,
+			success: function(result){
+				//alert(result);
+				if(result.indexOf("Success: ") !=-1){
+					//alert(result.substring(result.indexOf(" ")+1));
+					_profileimage="../profile_images/" + result.substring(result.indexOf(" ")+1);
+					$('#edit-picture-button').prop('disabled',false);
+					$('#cancel-edit-picture-button').prop('disabled',false);
+					
+					pictureDefaultView();
+				}
+				else{
+					alert('File could not be uploaded.  Please try again.');
+					$('#edit-picture-button').prop('disabled',false);
+					$('#cancel-edit-picture-button').prop('disabled',false);
+					$('#profileimage').attr('src', _profileimage);
+					pictureEditView();
+				}
+			}
+		});
+	}
 }
 
+function turnOffOtherEditFields(editButton){
+	// var buttonArray = $('.edit-button');
+	// for(var i = 0; i < buttonArray.length; i++){
+	// 	alert(buttonArray[i].attr('id'));
+	// }
+}
