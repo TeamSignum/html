1a334f27c919695d60d63d4b28238c09dd20f552
MedianJoe <probablyjoe@gmail.com>
2016-03-14 17:39:39 -0600

    Dynamic link changes and added bootstrap "active" class to active page link in the nav bar

--- navbar/navbar.js
+++ navbar/navbar.js
@@ -35,34 +35,52 @@ function evaluateSessionInfo(parsedResult){
 	// Navbar if user is not logged in
 	if(parsedResult=="Login"){
 		html+='<ul class="nav navbar-nav">';
+
 		// Login link
-		html+='<li>';
+		if(document.title=='Login'){
+			html+='<li class="active">';
+		}
+		else{
+			html+='<li>';
+		}
 		html+='<a href="../login/login.html" style="margin-top: 12px; font-size:14pt;">Login</a>';
 		html+='</li>';
+		
 		// Register link
-		html+='<li>';
+		if(document.title=='Registration'){ 
+			html+='<li class="active">';
+		}
+		else{
+			html+='<li>';
+		}
 		html+='<a href="../registration/registration.html" style="margin-top: 12px; font-size:14pt;">Register</a>';
 		html+='</li>';
 		
 		html+='</ul>';				
 		
 	}
+	
 	// Navbar if student is logged in
 	else if(parsedResult.role=='student'){
 		html+='<ul class="nav navbar-nav">';
+
 		// Account home link
-		html+='<li>';
+		if(document.title=='Account Home'){ 
+			html+='<li class="active">';
+		}
+		else{
+			html+='<li>';
+		}
 		html+='<a href="../student/student_main.html" style="margin-top: 12px; font-size:14pt;">Account Home</a>';
 		html+='</li>';
 
-		// Logic for grades links
-		// if(document.title=='student_main_page'){
-		// 	html+='<li>';
-		// 	html+='<a href="../grades/grade_charts.html" style="margin-top: 12px; font-size:14pt;">Grades</a>';
-		// 	html+='</li>';
-		// }
-		//else 
 		if(document.title=='Class View'){
+			// add class link and make it active
+			html+='<li class="active">';
+			html+='<a href="../class_view/class_view.html" style="margin-top: 12px; font-size:14pt;">'+
+			parsedResult.className+'</a>';
+			html+='</li>';
+			
 			html+='<li>';
 			html+='<a href="../grades/grades.html" style="margin-top: 12px; font-size:14pt;">Grades</a>';
 			html+='</li>';
@@ -70,58 +88,92 @@ function evaluateSessionInfo(parsedResult){
 
 		if(document.title=='Grades'){
 			html+='<li>';
-			html+='<a href="../class_view/class_view.html" style="margin-top: 12px; font-size:14pt;">Back to ' +
-			parsedResult.className +'</a>';
+			html+='<a href="../class_view/class_view.html" style="margin-top: 12px; font-size:14pt;">'+
+			parsedResult.className+'</a>';
+			html+='</li>';
+
+			html+='<li class="active">';
+			html+='<a href="../class_view/class_view.html" style="margin-top: 12px; font-size:14pt;">Grades</a>';
 			html+='</li>';
 		}
 
-		// Profile link
-		html+='<li>';
-		html+='<a href="../profile/profile.html" style="margin-top: 12px; font-size:14pt;">Profile</a>';
-		html+='</li>';	
+		// Profile link (only included on account home page, or the profile page itself)
+		if(document.title=='Account Home' || document.title=='Profile'){
+			if(document.title=='Profile'){
+				html+='<li class="active">';
+			}
+			else{
+				html+='<li>';
+			}
+			html+='<a href="../profile/profile.html" style="margin-top: 12px; font-size:14pt;">Profile</a>';
+			html+='</li>';
+		}	
+			
 		// Logout link
 		html+='<li>';
 		html+='<a href="../logout.php" style="margin-top: 12px; font-size:14pt;">Logout</a>';
 		html+='</li>';	
 
-		html+='</ul>';	
-		
+		html+='</ul>';
 	}
 	// Navbar if professor is logged in and on account home page
 	// **Currently contains all existing links for ease of testing other site features**
 	else if(parsedResult.role=='professor'){
 		html+='<ul class="nav navbar-nav">';
 		// Account home link
-		html+='<li>';
+		if(document.title=='Account Home'){ 
+			html+='<li class="active">';
+		}
+		else{
+			html+='<li>';
+		}
 		html+='<a href="../student/student_main.html" style="margin-top: 12px; font-size:14pt;">Account Home</a>';
 		html+='</li>';
-		// All grades chart link
-		// html+='<li>';
-		// html+='<a href="/grades/grade_charts.html" style="margin-top: 12px; font-size:14pt;">Grades</a>';
-		// html+='</li>';	
-		// Profile link
-		html+='<li>';
-		html+='<a href="../profile/profile.html" style="margin-top: 12px; font-size:14pt;">Profile</a>';
-		html+='</li>';
-		// Class View link
-		//html+='<li>';
-		//html+='<a href="../class_view/class_view.html" style="margin-top: 12px; font-size:14pt;">Class View</a>';
-		//html+='</li>';	
-		// Professor Class View
-		html+='<li>';
-		html+='<a href="../prof_class_view/prof_class_view.html" style="margin-top: 12px; font-size:14pt;">Professor Class View</a>';
-		html+='</li>';	
-		// Class Builder link
-		html+='<li>';
-		html+='<a href="../class_builder/class_builder.html" style="margin-top: 12px; font-size:14pt;">Class Builder</a>';
-		html+='</li>';		
+
+		if(document.title=='Class Builder'){
+			// add class link and make it active
+			html+='<li class="active">';
+			html+='<a href="../class_builder/class_builder.html" style="margin-top: 12px; font-size:14pt;">'+
+			parsedResult.className+' Builder</a>';
+			html+='</li>';
+
+			html+='<li>';
+			html+='<a href="../prof_class_view/prof_class_view.html" style="margin-top: 12px; font-size:14pt;">'+
+			parsedResult.className+' Class View</a>';
+			html+='</li>';
+		}
+
+		if(document.title=='Professor Class View'){
+			// add class link and make it active
+			html+='<li>';
+			html+='<a href="../class_builder/class_builder.html" style="margin-top: 12px; font-size:14pt;">'+
+			parsedResult.className+' Builder</a>';
+			html+='</li>';
+
+			html+='<li class="active">';
+			html+='<a href="../prof_class_view/prof_class_view.html" style="margin-top: 12px; font-size:14pt;">'+
+			parsedResult.className+' Class View</a>';
+			html+='</li>';
+		}
+		
+		// Profile link (only included on account home page, or the profile page itself)
+		if(document.title=='Account Home' || document.title=='Profile'){
+			if(document.title=='Profile'){
+				html+='<li class="active">';
+			}
+			else{
+				html+='<li>';
+			}
+			html+='<a href="../profile/profile.html" style="margin-top: 12px; font-size:14pt;">Profile</a>';
+			html+='</li>';
+		}	
+
 		// Logout link
 		html+='<li>';
 		html+='<a href="../logout.php" style="margin-top: 12px; font-size:14pt;">Logout</a>';
 		html+='</li>';	
 
 		html+='</ul>';	
-		;
 	}
 
 	insertNavBarHtml(html);
