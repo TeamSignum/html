8d1dfd771d51040ecc7204d4ca3ea8d7719e6caf
MedianJoe <probablyjoe@gmail.com>
2016-03-19 17:51:03 -0600

    Added grade submission capability
    All values are still hard coded, but functional.  Needs error checking
    and fix styling.

--- grades/professor_grading.js
+++ grades/professor_grading.js
@@ -16,10 +16,7 @@ $( document ).ready(function()
 {
 
 	$( "#p_Save" ).click(function() {
-		$("#custom_container").hide();
-		$("#popup").html("");
-		$("#canvas").show(); 
-		$("#dim_div").hide();
+		submitGrade()
 	});
 
 	$( "#p_Cancel" ).click(function() {
@@ -33,11 +30,12 @@ $( document ).ready(function()
 	
 });
 
-function getAssignments(parsedResult){
+function getAssignments(){
 	$.ajax({
 		async: true,
 		type: 'POST',
 		url: "professor_grading.php",
+		data: {'gradeQuery': 'getAssignments'},
 		datatpye: "json",
 		success: function(result){
 			//alert(result);
@@ -47,9 +45,31 @@ function getAssignments(parsedResult){
 	});
 }
 
+function submitGrade(){
+	// Get form data
+	var formData = $('#grade-form').serializeArray();
+	// Add the gradeQuery
+	formData.push({name:'gradeQuery', value:'submitGrade'});
+	//alert(formData);
+
+	$.ajax({
+		async: true,
+		type: 'POST',
+		url: "professor_grading.php",
+		data: formData,
+		success: function(result){
+			alert(result);
+			$("#custom_container").hide();
+			$("#popup").html("");
+			$("#canvas").show(); 
+			$("#dim_div").hide();
+		}
+	});
+}
+
 // POJO for storing the grade information to access in other functions without 
 // the need to make another database call
-function gradeData(userid, firstName, lastName, cid, nid, nid2, tdate, file, grade)
+function gradeData(userid, firstName, lastName, cid, nid, nid2, tdate, file, grade, studentComments, graderComments)
 {
 	this.userid = userid;
 	this.firstName = firstName;
@@ -60,6 +80,8 @@ function gradeData(userid, firstName, lastName, cid, nid, nid2, tdate, file, gra
 	this.tdate = tdate;
 	this.file = file;
 	this.grade = grade;
+	this.studentComments = studentComments;
+	this.graderComments = graderComments;
 }
 
 function buildAssignmentGradingPage(parsedResult){
@@ -89,8 +111,17 @@ function buildAssignmentGradingPage(parsedResult){
 		var tdate = parsedResult[i].tdate;
 		var file = parsedResult[i].file;
 		var grade = parsedResult[i].grade;
+		var studentComments;
+		if(parsedResult[i].student_comments != null){
+			studentComments = parsedResult[i].student_comments;
+		}
+		else{
+			studentComments = 'N/A';
+		}
 
-		var tempGradeData = new gradeData(userid, firstName, lastName, cid, nid, nid2, tdate, file, grade);
+		var graderComments = parsedResult[i].grader_comments;
+
+		var tempGradeData = new gradeData(userid, firstName, lastName, cid, nid, nid2, tdate, file, grade, studentComments, graderComments);
 		gradeDataArray[userid] = tempGradeData;
 
 		// Check to set row color to show graded vs ungraded
@@ -131,8 +162,8 @@ function buildAssignmentGradingPage(parsedResult){
 }	
 
 function insertTable(html){
-		$('#assignments').html(html);
-		initDataTable();
+	$('#assignments').html(html);
+	initDataTable();
 }
 
 function initDataTable(){
@@ -154,18 +185,30 @@ function createGradeForm(userid){
 	innerHtml = `
 	<div>
 		<div class="form-style-2" style="width: 90%;">
-		<div class="form-style-2-heading" style="width: 110%;">Grading for student: `+currentStudent.firstName+`</div>
-		<form style="margin-left: 13%;">
-	`;
+			<div class="form-style-2-heading" style="width: 110%;">Grading for student: `+currentStudent.firstName+`</div>
+				
+			<label for="assignment-submission">
+				<span>Assignment Submission</span>
+				<object id="assignment-submission" data="testPDF.pdf#page=1&zoom=75" type="application/pdf" width="100%" height="500px">
+			 	 	<p>Alternative text - include a link <a href="myfile.pdf">to the PDF!</a></p>
+				</object>
+			</label>
+			<label for="student-comments">
+				<span>Student Comments</span></span>
+				<textarea name="student-comments" id="student-comments" class="textarea-field" readonly>`+currentStudent.studentComments+`</textarea>
+			</label>
 
-	innerHtml += `
-		<label for="field1"><span>Title <span class="required">*</span></span><input class="input-field" id="title" name="title" type="text" value="Title"placeholder="Title"/></label>
-		<label for="field2"><span>Description <span class="required">*</span></span><input class="input-field" id="description" name="description" type="text" value="Assignment" placeholder="Description"/></label>
-		<label for="field5"><span>Notes <span class=""></span></span><textarea name="notes" id="notes" class="textarea-field">Student Comments</textarea></label>
-		<label for="field2"><span>Due Date<span class=""></span></span><input class="input-field" id="due_date" name="due_date" type="text" value="Grading Comments" placeholder="Due date"/></label>
-		`;
-
-	innerHtml += `
+			<form style="margin-left: 13%;" id="grade-form">
+				<label for="grader-comments">
+					<span>Grader Comments</span>
+					<input class="textarea-field" id="grader-comments" name="grader-comments" type="text">
+				</label>
+				<label for="grade">
+					<span>Grade</span>
+					<input class="input-field" id="grade" name="grade" type="text" required>
+				</label>
+			</form>
+		</div>
 	</div>
 	`;
 
--- grades/professor_grading.php
+++ grades/professor_grading.php
@@ -15,60 +15,121 @@
 	
 	require_once ('../db.php');
 
-	// Create a PDO object	
-	$DB=openDB();
-	
-	// if(isset($_POST['cid']) && isset($_POST['nid']) && isset($_POST['nid2'])){
-	// 	$cid = $_POST['cid'];
-	// 	$nid = $_POST['nid'];
-	// 	$nid2 = $_POST['nid2'];
-	// }
-	// else{
-	// 	die("Information required for query was not found.");
-	// }
+	if(isset($_POST['gradeQuery']))
+	{
+		$query=$_POST['gradeQuery'];
+		// Create a PDO object	
+		$DB=openDB();
 
-	// Hard coding values for testing
-	$cid="1";
-	$nid="1";
-	$nid2="1";
+		switch($query)
+		{
+			case 'getAssignments':
+			
+				// if(isset($_POST['cid']) && isset($_POST['nid']) && isset($_POST['nid2'])){
+				// 	$cid = $_POST['cid'];
+				// 	$nid = $_POST['nid'];
+				// 	$nid2 = $_POST['nid2'];
+				// }
+				// else{
+				// 	die("Information required for query was not found.");
+				// }
 
-	$query="SELECT u.firstname, 
-				   u.lastname, 
-				   u.idusers, 
-				   a.cid, 
-				   a.nid, 
-				   a.nid2, 
-				   a.tdate, 
-				   a.file, 
-				   a.grade
-			FROM users u
-			NATURAL JOIN enrolled e
-			LEFT JOIN assignments a
-				ON e.idusers = a.idusers
-			WHERE e.cid=? AND (a.nid=? OR a.nid IS NULL) AND (a.nid2=? OR a.nid2 IS NULL)
-			ORDER BY u.lastname ASC";
-						
-	$statement = $DB->prepare($query);
-	$statement->bindValue(1, $cid);					
-	$statement->bindValue(2, $nid);
-	$statement->bindValue(3, $nid2);	
-		
-	// Execute the database query
-	$statement->execute();
-	$result = $statement->fetchAll(PDO::FETCH_ASSOC);
-	
-	// TODO: Need error handling for empty query results
-	
-	// Check for a failed query
-	if(mysql_errno()){
-		echo "MySQL error ".mysql_errno().": "
- 		.mysql_error()."\n<br>When executing <br>\n$query\n<br>";
+				// Hard coding values for testing
+				$cid="1";
+				$nid="1";
+				$nid2="1";
+
+				$query="SELECT u.firstname, 
+							   u.lastname, 
+							   u.idusers, 
+							   a.cid, 
+							   a.nid, 
+							   a.nid2, 
+							   a.tdate, 
+							   a.file, 
+							   a.grade,
+							   a.student_comments,
+							   a.grader_comments
+						FROM users u
+						NATURAL JOIN enrolled e
+						LEFT JOIN assignments a
+							ON e.idusers = a.idusers
+						WHERE e.cid=? AND (a.nid=? OR a.nid IS NULL) AND (a.nid2=? OR a.nid2 IS NULL)
+						ORDER BY u.lastname ASC";
+									
+				$statement = $DB->prepare($query);
+				$statement->bindValue(1, $cid);					
+				$statement->bindValue(2, $nid);
+				$statement->bindValue(3, $nid2);	
+					
+				// Execute the database query
+				$statement->execute();
+				$result = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+				// TODO: Need error handling for empty query results
+				
+				// Check for a failed query
+				if(mysql_errno()){
+					echo "MySQL error ".mysql_errno().": "
+			 		.mysql_error()."\n<br>When executing <br>\n$query\n<br>";
+				}
+				else{
+					echo(json_encode($result));
+				}
+				break;
+
+			case 'submitGrade':
+				if(isset($_POST['grader-comments']) && isset($_POST['grade'])) {
+					$grader_comments = $_POST['grader-comments'];
+					$grade = $_POST['grade'];
+
+					echo ($grader_comments);
+					echo ($grade);
+				}
+				else{
+					die("Form data was not set.");
+				}
+
+				// Hard coding to test query
+
+				$query="UPDATE assignments
+						SET grader_comments=?, grade=?
+						WHERE cid=? AND nid=? AND nid2=? AND idusers=?";
+									
+				$statement = $DB->prepare($query);
+				$statement->bindValue(1, $grader_comments);					
+				$statement->bindValue(2, $grade);
+				$statement->bindValue(3, "1");
+				$statement->bindValue(4, "1");
+				$statement->bindValue(5, "1");
+				$statement->bindValue(6, "1");
+					
+				// Execute the database query
+				$statement->execute();
+				
+				// TODO: Need error handling for empty query results
+				
+				// Check for a failed query
+				if(mysql_errno()){
+					echo "MySQL error ".mysql_errno().": "
+			 		.mysql_error()."\n<br>When executing <br>\n$query\n<br>";
+				}
+				else{
+					echo("Success");
+				}
+
+				break;
+
+			default:
+				die("Unknown query type.");
+		}
+		// Close the PDO DB Connection (not necessary, but good practice...so I've read)
+		$DB = null;
 	}
 	else{
-		echo(json_encode($result));
+		//print_r($_POST);
+		die("Query type was not set.");
 	}
-	
-	// Close the PDO DB Connection (not necessary, but good practice...so I've read)
-	$DB = null;	
+
 
 ?>
