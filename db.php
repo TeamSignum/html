c804bfc5eeadb35ff3d824701de203e02d1acdce
MedianJoe <probablyjoe@gmail.com>
2016-03-19 16:39:52 -0600

    Added charset so json_encode doesn't break in php
    I added some names to the users table and for some reason this broke
    json_encode.  Added the charset parameter to fix this.

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 
 function openDB()
 {
-	$DB = new PDO('mysql:host=ec2-52-33-118-140.us-west-2.compute.amazonaws.com;dbname=LU', 'Signum', 'signumDB4');
+	$DB = new PDO('mysql:host=ec2-52-33-118-140.us-west-2.compute.amazonaws.com;dbname=LU;charset=utf8', 'Signum', 'signumDB4');
 	$DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	return $DB;
 }
