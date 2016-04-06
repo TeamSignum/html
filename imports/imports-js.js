02e1419c25ec9c7ff70fc5a27cb1d5084d69542a
MedianJoe <probablyjoe@gmail.com>
2016-04-06 00:19:11 -0600

    Added script loader to all pages
    Pages work in Chrome and Firefox.  To get all pages working in IE 10,
    need to remove any 'back-tick' syntax from javascript files as this is
    an ES6 feature that IE doesn't support yet.

--- imports/imports-js.js
+++ imports/imports-js.js
@@ -5,8 +5,10 @@
  * Date: Spring 2016 
  */ 
 
+// Load JQUERY first
 head.load({ jquery: "https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js" });
 
+// Once JQUERY loads, load remaining sitewide JS files
 head.ready(["jquery"], function() {
     head.load([{ bootstrapjs: "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" },
                { fabricjs: "http://cdnjs.cloudflare.com/ajax/libs/fabric.js/1.5.0/fabric.min.js" },
