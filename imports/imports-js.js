7605cc5f8d36037bb010f9cd3376f63621dea63c
MedianJoe <probablyjoe@gmail.com>
2016-04-06 18:46:00 -0600

    Changed script loader order
    Appears to have improved page loading speed.

--- imports/imports-js.js
+++ imports/imports-js.js
@@ -6,12 +6,13 @@
  */ 
 
 // Load JQUERY first
-head.load({ jquery: "https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js" });
+head.load([{ jquery: "https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js" },
+		   { fabricjs: "http://cdnjs.cloudflare.com/ajax/libs/fabric.js/1.5.0/fabric.min.js" }
+		   ]);
 
 // Once JQUERY loads, load remaining sitewide JS files
 head.ready(["jquery"], function() {
     head.load([{ bootstrapjs: "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" },
-               { fabricjs: "http://cdnjs.cloudflare.com/ajax/libs/fabric.js/1.5.0/fabric.min.js" },
                { sweetalertjs: "../imports/dist/sweetalert.min.js" },
                { helperjs: "../imports/helper.js" }],
                function() {
