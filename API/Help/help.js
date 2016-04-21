8f6bcc0443d1b268493d68fcc7e10b7c3035661d
Daniel Cushing <cushing.daniel.james@gmail.com>
2016-04-21 13:04:24 -0600

    Minor fix.

--- API/Help/help.js
+++ API/Help/help.js
@@ -15,12 +15,12 @@ function DisplayHelpMessage(page){
 			break;
 		case "class_view": 
 			help_message_title = "Class View";
-			help_message = "These are high level concepts. Click on nodes to navigate to the concept view. Each node has an associated pop up (page icon)." +
+			help_message = "These are high level concepts. Click on nodes to navigate to the concept view. \n\nEach node has an associated pop up (page icon)." +
 			" This pop up contains all information about the node.";
 			break;
 		case "concept_view": 
 			help_message_title = "Concept View";
-			help_message = "These are low level concepts. Each node has an associated pop up. Click the page icon to open up the pop up" +
+			help_message = "These are low level concepts. Each node has an associated pop up. \n\nClick the page icon to open up the pop up" +
 			" and see all information about the node.";
 			break;
 	}
